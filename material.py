45d16e289feafeebb8f9d54479d0e264c313f677
Tricky3d <111574146+Tricky3d@users.noreply.github.com>
2023-01-15 18:56:02 +0000

    khkh

--- material.py
+++ material.py
@@ -2,11 +2,11 @@ import chess
 import chess.polyglot
 import chess.svg
 import Pool
-
-
-
-
 board = chess.Board()
+
+
+
+
 highesteval = -10000
 bestmove = "e"
 def material_balance(board):
@@ -47,48 +47,53 @@ while True:
         board.push(chess.Move.from_uci(open))
         print(board)
 
-        board.push(chess.Move.from_uci(input("user move: ")))
+        board.push_san(input("user move: "))
         
     else:
+        evalboard = board
+        eval = 0
+        evalist = list()
+        evalist1 = list()
+        evalist2 = list()
+        evalist3 = list()
+        movelist = list()
         for i in range(board.legal_moves.count()):
             el = list(board.legal_moves)
-            evalboard = board
-            eval = 0
-            evalist = list()
-            movelist = list()
-            topmove = "e"
+            
+            
     
-            move = str(el[i-1])
+            move = str(el[i])
 
-            movelist.append(str(el[i-1]))
+            movelist.append(str(el[i]))
             
             evalboard.push(chess.Move.from_uci(move))
+            
             for i in range(evalboard.legal_moves.count()):
                 el = list(evalboard.legal_moves)
-                evalboard.push(chess.Move.from_uci(str(el[i-1])))
+                evalboard.push(chess.Move.from_uci(str(el[i])))
                 for i in range(evalboard.legal_moves.count()):
                     el = list(evalboard.legal_moves)
-                    evalboard.push(chess.Move.from_uci(str(el[i-1])))
+                    evalboard.push(chess.Move.from_uci(str(el[i])))
                     for i in range(evalboard.legal_moves.count()):
                         el = list(evalboard.legal_moves)
-                        evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                        for i in range(evalboard.legal_moves.count()):
-                            el = list(evalboard.legal_moves)
-                            evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                            eval = material_balance(evalboard)
-                            
-                            evalist.append(eval)
-                            evalboard.pop()
+                        evalboard.push(chess.Move.from_uci(str(el[i])))
+                        eval = material_balance(evalboard)
+                        
+                        evalist.append(eval)
                         evalboard.pop()
+                    evalist1.append(evalist[evalist.index(min(evalist))])
                     evalboard.pop()
+                evalist2.append(evalist1[evalist1.index(max(evalist1))])
                 evalboard.pop()
+            evalist3.append(evalist2[evalist2.index(min(evalist2))])
             evalboard.pop()
+        bestmove = movelist[evalist3[evalist3.index(max(evalist3))]]
         board.push(chess.Move.from_uci(bestmove))
         print(board)
         boardsvg = chess.svg.board(board, size=300) 
 
        
-        board.push(chess.Move.from_uci(input("user move: ")))
+        board.push_san(input("user move: "))
         print(board)
         
 
--- test.py
+++ test.py
@@ -1,29 +1,60 @@
 import chess
 
 board = chess.Board()
+evalboard = board
+eval = 0
+evalist = list()
+evalist1 = list()
+evalist2 = list()
+evalist3 = list()
+movelist = list()
 
+board.push_san("e4")
+board.push_san("e5")
+board.push_san("Nf3")
+board.push_san("Ke7")
+
+
+
+
+def material_balance(board):
+    white = board.occupied_co[chess.WHITE]
+    black = board.occupied_co[chess.BLACK]
+    return (
+        chess.popcount(white & board.pawns) - chess.popcount(black & board.pawns) +
+        3 * (chess.popcount(white & board.knights) - chess.popcount(black & board.knights)) +
+        3 * (chess.popcount(white & board.bishops) - chess.popcount(black & board.bishops)) +
+        5 * (chess.popcount(white & board.rooks) - chess.popcount(black & board.rooks)) +
+        9 * (chess.popcount(white & board.queens) - chess.popcount(black & board.queens))
+    )
 for i in range(board.legal_moves.count()):
-            el = list(board.legal_moves)
-            evalboard = board
-            eval = 0
-    
-            move = str(el[i-1])
-            
-            evalboard.push(chess.Move.from_uci(move))
+    el = list(board.legal_moves)
+    evalboard = board
+
+
+    move = str(el[i])
+
+    movelist.append(str(el[i]))
+
+    evalboard.push(chess.Move.from_uci(move))
+    for i in range(evalboard.legal_moves.count()):
+        el = list(evalboard.legal_moves)
+        evalboard.push(chess.Move.from_uci(str(el[i])))
+        for i in range(evalboard.legal_moves.count()):
+            el = list(evalboard.legal_moves)
+            evalboard.push(chess.Move.from_uci(str(el[i])))
             for i in range(evalboard.legal_moves.count()):
                 el = list(evalboard.legal_moves)
-                evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                for i in range(evalboard.legal_moves.count()):
-                    el = list(evalboard.legal_moves)
-                    evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                    for i in range(evalboard.legal_moves.count()):
-                        el = list(evalboard.legal_moves)
-                        evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                        for i in range(evalboard.legal_moves.count()):
-                            el = list(evalboard.legal_moves)
-                            evalboard.push(chess.Move.from_uci(str(el[i-1])))
-                            evalboard.pop()
-                        evalboard.pop()
-                    evalboard.pop()
+                evalboard.push(chess.Move.from_uci(str(el[i])))
+                eval = material_balance(evalboard)
+                
+                evalist.append(eval)
                 evalboard.pop()
+            evalist1.append(evalist[evalist.index(min(evalist))])
             evalboard.pop()
+        evalist2.append(evalist1[evalist1.index(max(evalist1))])
+        evalboard.pop()
+    evalist3.append(evalist2[evalist2.index(min(evalist2))])
+    evalboard.pop()
+bestmove = movelist[evalist3[evalist3.index(max(evalist3))]]
+print(bestmove)
